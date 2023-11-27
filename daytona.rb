79b17143a52f189f86b66213d80a03deeb291084
Zoran Zorica <zoran.zorica@gmail.com>
2023-11-27 14:47:15 +0100

    feat: update brew package version to 8.60.0

--- daytona.rb
+++ daytona.rb
@@ -1,20 +1,20 @@
 class Daytona < Formula
   desc "Daytona CLI"
   homepage "https://daytona.io"
-  version "8.56.0"
+  version "8.60.0"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://download.daytona.io/cli/v#{version}/daytona-darwin-arm64"
-    sha256 "04dcbdb3965867aa39596edd694002e7022c9c0ee9fd38da1f41c94b283c4a83"
-  elsif OS.mac? && Hardware::CPU.intel?    
-    url "https://download.daytona.io/cli/v#{version}/daytona-darwin-amd64"    
-    sha256 "5415f5cfe9cab996ec476ffaf220ef713e6e008e6be9cc95b09c7a8e767dde88"
+    sha256 "a61f73e28abfba9652466df905d24b0424e38636aebf2f81cce7b0c82f40ba89"
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://download.daytona.io/cli/v#{version}/daytona-darwin-amd64"
+    sha256 "7040f78ad83083dbacaabe508f0ea42df64e1c131524eae4384845cf61008f98"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://download.daytona.io/cli/v#{version}/daytona-linux-amd64"
-    sha256 "7196db468f75d7aa9df89f316cf32ce341aa7f68d9f5c5b83d701139f3fc5bae"
+    sha256 "ac37044035a39f7be135a879202879728b956f320e35a08512beae2bd09c15d1"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://download.daytona.io/cli/v#{version}/daytona-linux-arm64"
-    sha256 "92262e0bf4f91affc8e0b544930379af06bb61e51dbf919e70c55c6acc39361a"
+    sha256 "fb273756e10000978ff6a01c50afed9dcd8f99c8a075fcdf5b02c0fff61f530b"
   else
     odie "Unsupported OS/ARCH combination"
   end
