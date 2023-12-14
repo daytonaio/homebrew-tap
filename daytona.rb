e7adea2dadea82894143833c9501b3fa6003680c
Zoran Zorica <zoran.zorica@gmail.com>
2023-12-14 14:47:45 +0100

    fix: version variable in url

--- daytona.rb
+++ daytona.rb
@@ -4,16 +4,16 @@ class Daytona < Formula
   version "8.68.0"
 
   if OS.mac? && Hardware::CPU.arm?
-  url "https://download.daytona.io/cli/v8.64.1/daytona-darwin-arm64"
+  url "https://download.daytona.io/cli/v#{version}/daytona-darwin-arm64"
   sha256 "9825d3668c8dececf7050c527b3a7ab668c0675fb54bbf47c7137eeaaa9045f0"
   elsif OS.mac? && Hardware::CPU.intel?
-  url "https://download.daytona.io/cli/v8.64.1/daytona-darwin-amd64"
+  url "https://download.daytona.io/cli/v#{version}/daytona-darwin-amd64"
   sha256 "a698e6d104a0c27b92402326805b8a0edfb841fecef5f13cc208eee3a0d5e167"
   elsif OS.linux? && Hardware::CPU.intel?
-  url "https://download.daytona.io/cli/v8.64.1/daytona-linux-amd64"
+  url "https://download.daytona.io/cli/v#{version}/daytona-linux-amd64"
   sha256 "dc6f77ca29c8863fcd64be3d5c6c9bd1f484e5ba21467738b346a8307ebfa789"
   elsif OS.linux? && Hardware::CPU.arm?
-  url "https://download.daytona.io/cli/v8.64.1/daytona-linux-arm64"
+  url "https://download.daytona.io/cli/v#{version}/daytona-linux-arm64"
   sha256 "750ddf3c882e482cd7084d10df8fe8ce4551594fc4c4abbe8d5a0bec8216f0e7"
   else
     odie "Unsupported OS/ARCH combination"
