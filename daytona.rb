becd5eb1fd12df3faaef52f4aaf7cf55a5f3d0c5
Ivan Dagelić <dagelic.ivan@gmail.com>
2024-05-17 16:23:24 +0200

    chore: switch to oss (#6)
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- daytona.rb
+++ daytona.rb
@@ -1,20 +1,20 @@
 class Daytona < Formula
   desc "Daytona CLI"
   homepage "https://daytona.io"
-  version "8.88.0"
+  version "0.16.0"
 
   if OS.mac? && Hardware::CPU.arm?
-  url "https://download.daytona.io/cli/v#{version}/daytona-darwin-arm64"
-  sha256 "c49935b1162784cd2ba89d72aeaea7f1340fa4e7132d41fc3f8af3b8deb01c8a"
+  url "https://download.daytona.io/daytona/v#{version}/daytona-darwin-arm64"
+  sha256 "aefd57fe4e9fd707661d357e21d9cb354a428e9ed3d5131809a0396d4b31f1e6"
   elsif OS.mac? && Hardware::CPU.intel?
-  url "https://download.daytona.io/cli/v#{version}/daytona-darwin-amd64"
-  sha256 "4b825516de699f9b92676f98376590e9f57f0df477f1454b85ca9421521b3f53"
+  url "https://download.daytona.io/daytona/v#{version}/daytona-darwin-amd64"
+  sha256 "bc6e2b4e6638356c7a51bfaca5d973abf6b42bd30359caa0fb9defe5b8a0361c"
   elsif OS.linux? && Hardware::CPU.intel?
-  url "https://download.daytona.io/cli/v#{version}/daytona-linux-amd64"
-  sha256 "2dbac2406dd501f7d92d439b0f792ef25339a6fbd76d84032f78d381e9ef49e2"
+  url "https://download.daytona.io/daytona/v#{version}/daytona-linux-amd64"
+  sha256 "013a85f090f5be45f556e8a70a91746bb038b3262c092547d85ef03e5984dfdb"
   elsif OS.linux? && Hardware::CPU.arm?
-  url "https://download.daytona.io/cli/v#{version}/daytona-linux-arm64"
-  sha256 "6a02aedda678d5ad52ae654d3740da8909c2de2bbfc104884029c333d8923da7"
+  url "https://download.daytona.io/daytona/v#{version}/daytona-linux-arm64"
+  sha256 "429881c61535b53dbb2ff94b32a26de490ed3caabd5033366f515712b2409629"
   else
     odie "Unsupported OS/ARCH combination"
   end
