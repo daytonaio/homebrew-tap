8ced7d384b119a928e7019ac33f7f8d81f8ce3d1
daytonaBot <daytonaBot@users.noreply.github.com>
2024-06-07 16:01:33 +0000

    Automated file update to - v0.18.0

--- daytona.rb
+++ daytona.rb
@@ -11,10 +11,10 @@ class Daytona < Formula
   sha256 "a1f08631325637f11aeab0e41457d0709fe3c5d20b7877a897c0dc6bb4b998f6"
   elsif OS.linux? && Hardware::CPU.intel?
   url "https://download.daytona.io/daytona/v#{version}/daytona-linux-amd64"
-  sha256 "87127a70c9222e31f76f494711a899a2aea858a0c50658978dd61b83a3343188"
+  sha256 "26170e16d0c6abfe55f427587410289a0e1f9d2eac5d6146b333b59d8e52c5e8"
   elsif OS.linux? && Hardware::CPU.arm?
   url "https://download.daytona.io/daytona/v#{version}/daytona-linux-arm64"
-  sha256 "ae57447f5b0bf9aa26cd92b26ebc18d25b795bf1a2215deed4b0156be7bbcb31"
+  sha256 "3654580856b8fdf22563f87758b12ed392d8e41fba1643831082fe657cca5411"
   else
     odie "Unsupported OS/ARCH combination"
   end
